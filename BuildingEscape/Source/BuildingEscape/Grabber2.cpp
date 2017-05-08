8775a09fc14573f56fe7d8e28b7a8b744a73c7dd
DESKTOP-971TE63\Schoe <Schoelsy@wp.pl>
2017-05-08 14:52:12 +0200

    GrabAndBindKeys

--- BuildingEscape/Source/BuildingEscape/Grabber2.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber2.cpp
@@ -20,12 +20,38 @@ UGrabber2::UGrabber2()
 void UGrabber2::BeginPlay()
 {
 	Super::BeginPlay();
-
 	UE_LOG(LogTemp, Warning, TEXT("Grabber on duty!"));
+
+	/// Look for attached Physics Handle
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (PhysicsHandle)
+	{
+		// Physics handle is found
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("No Physics Handle component for: %s"), *GetOwner()->GetName());
+	}
+
+	/// Look for attached Input component (only appears at run time)
 	
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if (InputComponent)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Input Component found"));
+		/// Bind the input axis
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber2::Grab);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("No Input component for: %s"), *GetOwner()->GetName());
+	}
 }
 
+void UGrabber2::Grab() {
 
+	UE_LOG(LogTemp, Warning, TEXT("Grab pressed"));
+}
 // Called every frame
 void UGrabber2::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
--- BuildingEscape/Source/BuildingEscape/Grabber2.h
+++ BuildingEscape/Source/BuildingEscape/Grabber2.h
@@ -26,4 +26,11 @@ public:
 private:
 	// How far of the player can we reach
 	float Reach = 100.f;
+
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	
+	UInputComponent* InputComponent = nullptr;
+
+	// Ray-cast and grab what's in reach
+	void Grab();
 };
